bb0f99e5000b185134010c119cefd7dc51b91202
Stephen Goncalves <stephengoncalves@pop-os.localdomain>
2023-09-21 02:34:56 +0100

    serve html&files, styles & hotreload

--- main.go
+++ main.go
@@ -3,12 +3,31 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"text/template"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+
+	workDir, _ := os.Getwd()
+	staticFiles := http.Dir(filepath.Join(workDir, "assets"))
+
+	router := chi.NewRouter()
+
+	router.Use(middleware.Logger)
+
+	FileServer(router, "/assets", staticFiles)
+
+	router.Get("/", indexHandler)
+
 	server := &http.Server{
 		Addr:    ":8080",
-		Handler: http.HandlerFunc(handler),
+		Handler: router,
 	}
 
 	err := server.ListenAndServe()
@@ -18,6 +37,26 @@ func main() {
 	}
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World!"))
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	template := template.Must(template.ParseFiles("index.html"))
+	template.Execute(w, nil)
+}
+
+func FileServer(r chi.Router, path string, root http.FileSystem) {
+	if strings.ContainsAny(path, "{}*") {
+		panic("FileServer does not permit any URL parameters.")
+	}
+
+	if path != "/" && path[len(path)-1] != '/' {
+		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
+		path += "/"
+	}
+	path += "*"
+
+	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
+		rctx := chi.RouteContext(r.Context())
+		pathPrefix := strings.TrimSuffix(rctx.RoutePattern(), "/*")
+		fs := http.StripPrefix(pathPrefix, http.FileServer(root))
+		fs.ServeHTTP(w, r)
+	})
 }
